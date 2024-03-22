89ffb7ad3ba442c585564810c68499d5e7e2dfa7
Icaro Caetano <icaro.figueiredo.atos@elo.com.br>
2024-03-22 10:26:26 -0300

    feat: Inclusao de especificacao dos Inputs

--- src/main/java/br/com/southsystem/cooperativism/application/request/dto/AssociateRequest.java
+++ src/main/java/br/com/southsystem/cooperativism/application/request/dto/AssociateRequest.java
@@ -2,17 +2,21 @@ package br.com.southsystem.cooperativism.application.request.dto;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Responsável pelo Associado")
 public class AssociateRequest {
 	
+	@Schema(description = "Entrada de dados para inclusão do nome do associado", defaultValue="Machado de Assis")
 	@Size(min = 3, max = 30, message = "Nome deve conter entre 3 e 30 caracteres")
 	@NotBlank(message = "Nome deve conter entre 3 e 30 caracteres")
 	@Pattern(regexp = "^[A-Za-zÀ-ú ]*$", message = "Nome deve conter apenas letras")
 	private String name;
 	
+	@Schema(description = "Entrada de dados para inclusão do cpf do associado", defaultValue="46735825004")
 	@Size(min = 11, max = 11, message = "CPF deve conter 11 dígitos")
     @NotBlank(message = "CPF não deve ser nulo")
     @CPF(message = "Insira um CPF válido")
