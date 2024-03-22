b12d767be5f411dbdb78eba97ac467adcde31e7c
Icaro Caetano <icaro.figueiredo.atos@elo.com.br>
2024-03-22 10:18:05 -0300

    feat: Inclusao de especificacao dos Inputs

--- src/main/java/br/com/southsystem/cooperativism/application/request/dto/SessionRequest.java
+++ src/main/java/br/com/southsystem/cooperativism/application/request/dto/SessionRequest.java
@@ -1,12 +1,16 @@
 package br.com.southsystem.cooperativism.application.request.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "Responsável pela Sessão")
 public class SessionRequest {
 	
 	@NotBlank
+	@Schema(description = "Entrada de dados para inclusão do cpf do Associado ", defaultValue="46735825004")
 	private String agendaId;
 	
+	@Schema(description = "Entrada de dados para inclusão do tempo da sessão em minutos.", defaultValue="10")
 	private Integer sessionTime;
 
 	private SessionRequest() {
