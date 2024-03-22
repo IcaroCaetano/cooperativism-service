bf1fa5bd1b367ab25941bbfeb23a759893e486fb
Icaro Caetano <icaro.figueiredo.atos@elo.com.br>
2024-03-22 10:31:18 -0300

    feat: Inclusao de especificacao dos Inputs

--- src/main/java/br/com/southsystem/cooperativism/application/request/dto/AgendaRequest.java
+++ src/main/java/br/com/southsystem/cooperativism/application/request/dto/AgendaRequest.java
@@ -1,14 +1,18 @@
 package br.com.southsystem.cooperativism.application.request.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Responsável pelo Pauta")
 public class AgendaRequest {
 	
+	@Schema(description = "Entrada de dados para inclusão do código da associado. O código não pode ser duplicado.", defaultValue="1")
 	@Nonnull
 	private Integer code;
 	
+	@Schema(description = "Entrada de dados para inclusão da descrição da Pauta.", defaultValue="PAUTA 001 de Implantação de Sistema de gestão")
 	@Size(min = 2, max = 50, message = "Nome deve conter entre 2 e 50 caracteres")
 	@NotBlank
 	private String description;
